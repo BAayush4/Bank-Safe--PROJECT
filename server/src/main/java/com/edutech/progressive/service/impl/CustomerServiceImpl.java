cafaa678cac5e97b54437be16405b75d5d913d45
BansalAbhinav <dummy@gmail.com>
2026-05-09 15:46:15 +0000

    frontend and backend fixes

--- server/src/main/java/com/edutech/progressive/service/impl/CustomerServiceImpl.java
+++ server/src/main/java/com/edutech/progressive/service/impl/CustomerServiceImpl.java
@@ -1,54 +1,96 @@
 package com.edutech.progressive.service.impl;
 
-import com.edutech.progressive.dao.CustomerDAO;
+
 import com.edutech.progressive.entity.Customers;
+import com.edutech.progressive.exception.CustomerAlreadyExistsException;
+import com.edutech.progressive.repository.AccountRepository;
+import com.edutech.progressive.repository.CustomerRepository;
+import com.edutech.progressive.repository.TransactionRepository;
 import com.edutech.progressive.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+@Service
 public class CustomerServiceImpl implements CustomerService {
 
-    CustomerDAO customerDAO;
-    private static List<Customers> customersList = new ArrayList<>();
+    @Autowired
+    PasswordEncoder passwordEncoder;
 
-    public CustomerServiceImpl(CustomerDAO customerDAO) {
-        this.customerDAO = customerDAO;
+    @Autowired
+    TransactionRepository transactionRepository;
+
+    @Autowired
+    AccountRepository accountRepository;
+
+    private final CustomerRepository customerRepository;
+
+    @Autowired
+    public CustomerServiceImpl(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
     }
 
     @Override
     public List<Customers> getAllCustomers() throws SQLException {
-        return customerDAO.getAllCustomers();
+        return customerRepository.findAll();
     }
 
     @Override
     public Customers getCustomerById(int customerId) throws SQLException {
-        return customerDAO.getCustomerById(customerId);
+        return customerRepository.findByCustomerId(customerId);
     }
 
     @Override
     public int addCustomer(Customers customers) throws SQLException {
-        return customerDAO.addCustomer(customers);
+        Customers existingCustomer = customerRepository.findByEmail(customers.getEmail());
+        if (existingCustomer != null) {
+            throw new CustomerAlreadyExistsException("Customer with give email already exists : " + customers.getEmail());
+        }
+        Customers oldUser = customerRepository.findByUsername(customers.getUsername());
+        if (oldUser != null) {
+            throw new CustomerAlreadyExistsException("Customer with give username already exists : " + customers.getUsername());
+        }
+        customers.setPassword(passwordEncoder.encode(customers.getPassword()));
+        return customerRepository.save(customers).getCustomerId();
     }
 
     @Override
     public void updateCustomer(Customers customers) throws SQLException {
-        customerDAO.updateCustomer(customers);
+        Customers existingCustomer = customerRepository.findByEmail(customers.getEmail());
+        if (existingCustomer != null && customers.getCustomerId() != existingCustomer.getCustomerId()) {
+            throw new CustomerAlreadyExistsException("Customer with give email already exists : " + customers.getEmail());
+        }
+        Customers oldUser = customerRepository.findByUsername(customers.getUsername());
+        if (oldUser != null && customers.getCustomerId() != oldUser.getCustomerId()) {
+            throw new CustomerAlreadyExistsException("Customer with give username already exists : " + customers.getUsername());
+        }
+        if (!customers.getRole().isBlank()) {
+            if (!existingCustomer.getPassword().equals(customers.getPassword())) {
+                customers.setPassword(passwordEncoder.encode(customers.getPassword()));
+            }
+            customerRepository.save(customers);
+        } else {
+            throw new SQLException("Role for a customer cannot be empty");
+        }
     }
 
     @Override
+    @Transactional
     public void deleteCustomer(int customerId) throws SQLException {
-        customerDAO.deleteCustomer(customerId);
+        transactionRepository.deleteByCustomerId(customerId);
+        accountRepository.deleteByCustomerId(customerId);
+        customerRepository.deleteByCustomerId(customerId);
     }
 
     @Override
     public List<Customers> getAllCustomersSortedByName() throws SQLException {
-        List<Customers> sortedCustomers = customerDAO.getAllCustomers();
-        if (sortedCustomers != null) {
-            Collections.sort(sortedCustomers);
-        }
+        List<Customers> sortedCustomers = customerRepository.findAll();
+        Collections.sort(sortedCustomers);
         return sortedCustomers;
     }
 }
